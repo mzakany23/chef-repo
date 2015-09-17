7621b5c1f45d327930f03cbf9148a9298e4ba58c
Michael Zakany <mzakany@gmail.com>
2015-09-17 09:55:02 -0400

    m

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,1 +1,11 @@
 # See https://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "mzakany23"
+client_key               "#{current_dir}/mzakany23.pem"
+validation_client_name   "mzakany23-validator"
+validation_key           "#{current_dir}/mzakany23-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/mzakany23"
+cookbook_path            ["#{current_dir}/../cookbooks"]
